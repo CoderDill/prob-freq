88b7a729d414a4e44b4621e5bbe588436eae6602
CoderDill <mddillon47@yahoo.com>
2022-01-04 11:04:08 -0800

    constructNote

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,6 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+    //Should return true if letters can build msg, otherwise false.
+       
+
 }

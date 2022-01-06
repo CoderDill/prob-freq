7b9bda8e07bddb12cb11c37106c892c044a98919
CoderDill <mddillon47@yahoo.com>
2022-01-06 13:47:22 -0800

    averagePair done

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,18 @@
 // add whatever parameters you deem necessary
 function averagePair(sortedArr, targetAvg) {
-    
+  let leftIdx = 0;
+  let rightIdx = sortedArr.length - 1;
+
+  while (leftIdx < rightIdx) {
+    const avg = (sortedArr[leftIdx] + sortedArr[rightIdx]) / 2;
+    if (avg === targetAvg) return true;
+    if (avg < targetAvg) {
+      leftIdx++;
+    } else {
+      rightIdx--;
+    }
+  }
+  return false;
 }
+
+averagePair([-1, 0, 3, 4, 5, 6], 4.5);

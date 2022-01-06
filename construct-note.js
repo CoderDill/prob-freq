b7c34035b0e7228b452f0e4462f9a4ff20313c1b
CoderDill <mddillon47@yahoo.com>
2022-01-06 13:18:31 -0800

    construct-note done

--- construct-note.js
+++ construct-note.js
@@ -1,6 +1,19 @@
 // add whatever parameters you deem necessary
 function constructNote(msg, letters) {
-    //Should return true if letters can build msg, otherwise false.
-       
+  //Should return true if letters can build msg, otherwise false.
+  let msgObj = {};
+  let lettersObj = {};
 
+  for (let char of msg) {
+    msgObj[char] = msgObj[char] + 1 || 1;
+    }
+    for (let char of letters) {
+    lettersObj[char] = lettersObj[char] + 1 || 1;
+  }
+    console.log(msgObj, lettersObj)
+    for (let key in msgObj) {
+        if (lettersObj[key] !== msgObj[key]) return false;
+
+    }
+    return true;
 }

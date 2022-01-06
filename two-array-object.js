b807f6999e0887e874a2ad4e988df4faf0b3ab56
Matthew Dillon <mddillon47@yahoo.com>
2022-01-06 15:23:53 -0800

    Update two-array-object.js

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrObj(arr1, arr2) {
+    let arrObj = {}
+    let count = 0
+
+    for (let key of arr1) {
+        arrObj[key] = arr2[count] || null
+        count++
+    }
+    return arrObj
 }
+

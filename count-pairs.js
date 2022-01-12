4e301d8fab53537294a036471bbb6ad3b28fbdc5
CoderDill <mddillon47@yahoo.com>
2022-01-12 12:28:21 -0800

    count pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,17 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, target, start = 0, end = arr.length - 1) {
+  arr.sort((a, b) => a - b);
+  let count = 0;
+  while (start < end) {
+    let sum = arr[start] + arr[end];
+    if (sum === target) {
+      count++;
+      start++;
+      end--;
+    } else if (sum < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return count;
 }
